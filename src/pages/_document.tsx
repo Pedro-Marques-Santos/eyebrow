aaae980702668b0727f466d4d3b949b5f067060f
Pedro-Marques-Santos <pedromarquesnoot@outlook.com>
2023-05-30 15:03:21 -0300

    test metatag 8

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,7 +24,7 @@ function MyDocument(props: any) {
 
         <meta
           name="description"
-          content="Aprecie a beleza dos seus olhos com serviços e produtos de alta qualidade que uma profissional de sobrancelhas pode fornecer a você"
+          content="Aprecie a beleza dos seus olhos com serviços e produtos de alta qualidade que uma Design de sobrancelhas pode fornecer a você"
         />
 
         <meta property="og:url" content="https://eyebrow.vercel.app/" />
@@ -32,19 +32,25 @@ function MyDocument(props: any) {
         <meta property="og:title" content="Studio Sardeli" />
         <meta
           property="og:description"
-          content="Aprecie a beleza dos seus olhos com serviços e produtos de alta qualidade que uma profissional de sobrancelhas pode fornecer a você"
+          content="Aprecie a beleza dos seus olhos com serviços e produtos de alta qualidade que uma Design de sobrancelhas pode fornecer a você"
         />
-        <meta property="og:image" content="/logostudio.jpg" />
+        <meta
+          property="og:image"
+          content="https://img.freepik.com/free-photo/young-woman-getting-beauty-treatment-her-eyebrows_23-2148910543.jpg?w=740&t=st=1685469501~exp=1685470101~hmac=6f763189dee14b9473257787390507753df4c7daf9affe99f19f8568ffaad29e"
+        />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="eyebrow.vercel.app" />
-        <meta property="twitter:url" content="eyebrow.vercel.app" />
+        <meta property="twitter:url" content="https://eyebrow.vercel.app/" />
         <meta name="twitter:title" content="Studio Sardeli" />
         <meta
           name="twitter:description"
-          content="Aprecie a beleza dos seus olhos com serviços e produtos de alta qualidade que uma profissional de sobrancelhas pode fornecer a você"
+          content="Aprecie a beleza dos seus olhos com serviços e produtos de alta qualidade que uma Design de sobrancelhas pode fornecer a você"
         />
-        <meta name="twitter:image" content="/logostudio.jpg" />
+        <meta
+          name="twitter:image"
+          content="https://img.freepik.com/free-photo/young-woman-getting-beauty-treatment-her-eyebrows_23-2148910543.jpg?w=740&t=st=1685469501~exp=1685470101~hmac=6f763189dee14b9473257787390507753df4c7daf9affe99f19f8568ffaad29e"
+        />
       </Head>
       <body>
         <Main />
